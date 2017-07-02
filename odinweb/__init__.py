d7d775acebb6018327933bc1a8f7b48c13498fd8
Tim Savage <tim@savage.company>
2017-07-03 01:51:33 +1000

    Added version info, notes on Python 2.7

--- odinweb/__init__.py
+++ odinweb/__init__.py
@@ -1,2 +1,13 @@
+"""
+Odin Web
+~~~~~~~~
+
+Web APIs utilising Odin for encoding/decoding and validation.
+
+"""
 __import__('pkg_resources').declare_namespace(__name__)
-__version__ = 0.1
+
+__authors__ = "Tim Savage"
+__author_email__ = "tim@savage.company"
+__copyright__ = "Copyright (C) 2016-2017 Tim Savage"
+__version__ = "0.1"
--- odinweb/_compat.py
+++ odinweb/_compat.py
@@ -1,11 +1,11 @@
+# -*- coding: utf-8 -*-
 """
-Py27
-~~~~
+Py27 Support
+~~~~~~~~~~~~
 
-Even though Lambda only officially supports Python 2.7 this library is being developed Python 3.x first. This module
-provides Python 2.7 fall-backs where possible.
+Like odin this library will support Python 2.7 through to version 2.0.
 
-This library assumes Python 2.7 if the version is less than 3.
+From this point onwards Python 3.5+ will be required.
 
 """
 from __future__ import unicode_literals
@@ -15,7 +15,7 @@ import sys
 __all__ = (
     'PY2', 'PY3',
     'string_types', 'integer_types', 'text_type', 'binary_type',
-    'range'
+    'range', 'with_metaclass'
 )
 
 PY2 = sys.version_info[0] == 2
