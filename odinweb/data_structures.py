9506a44531ac98aa5f017228d650388739dfb5e1
Tim Savage <tim.savage@westpacdatabank.com.au>
2017-07-07 19:18:04 +1000

    Removed the route number not needed in this implementation

--- odinweb/data_structures.py
+++ odinweb/data_structures.py
@@ -1,9 +1,8 @@
 from collections import namedtuple
-from itertools import chain
 
 
 # Used to define path nodes
 PathNode = namedtuple('PathNode', ('name', 'type', 'type_args'))
 
 # Generic definition for a route to an API endpoint
-ApiRoute = namedtuple("ApiRoute", ('route_number', 'path', 'methods', 'callback'))
+ApiRoute = namedtuple("ApiRoute", ('path', 'methods', 'callback'))
