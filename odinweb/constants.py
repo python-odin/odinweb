2bcf966107535649d603df895f29f11fd620b512
Tim Savage <tim@savage.company>
2017-07-10 16:02:14 +1000

    Added Swagger constants

--- odinweb/constants.py
+++ odinweb/constants.py
@@ -10,3 +10,18 @@ OPTIONS = 'OPTIONS'
 # Type of path
 PATH_TYPE_COLLECTION = 'collection'
 PATH_TYPE_RESOURCE = 'resource'
+
+# Parameter location
+IN_PATH = 'path'
+IN_QUERY = 'query'
+IN_HEADER = 'header'
+IN_BODY = 'body'
+IN_FORM = 'formData'
+
+# Data-types
+TYPE_STRING = "string"
+TYPE_NUMBER = "number"
+TYPE_INTEGER = "integer"
+TYPE_BOOLEAN = "boolean"
+TYPE_ARRAY = "array"
+TYPE_FILE = "file"
