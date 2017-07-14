466901641dbe442c678ababf5868bf5ddef248f6
Tim Savage <tim@savage.company>
2017-07-14 15:25:25 +1000

    Migrated to PBR and fixed inclusion of static files

--- setup.py
+++ setup.py
@@ -1,47 +1,7 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#!/usr/bin/env python
+from setuptools import setup
 
 setup(
-    name='odinweb',
-    namespace_packages=['odinweb'],
-    version='0.1',
-    description="Toolkit for building web API's using Odin",
-    long_description=long_description,
-    url='https://github.com/python-odin/odinweb',
-    author='Tim Savage',
-    author_email='tim@savage.company',
-    license='BSD',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-
-        'License :: OSI Approved :: BSD License',
-
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-
-    keywords='odin web rest api',
-
-    packages=find_packages(include=('odinweb',)),
-
-    install_requires=['odin>=1.0'],
+    setup_requires=['pbr>=2.0', 'setuptools>=17.1'],
+    pbr=True,
 )
