1560ae2c380b2df8c058f7756bed1e26689e49cc
Tim Savage <tim@savage.company>
2017-07-27 17:34:28 +1000

    Made the doc operations the sources of truth for doc types and build out so order of decorators shouldn't matter

--- odinweb/doc.py
+++ odinweb/doc.py
@@ -7,52 +7,46 @@ Additional decorators for improving documentation of APIs.
 """
 from collections import defaultdict
 from typing import Any
+
 from odin.utils import getmeta
-from odinweb.utils import dict_filter, dict_filter_update
 
 from . import _compat
 from .constants import In, HTTPStatus
+from .utils import dict_filter_update, dict_filter
 
 __all__ = (
-    # Docs
-    'OperationDoc', 'operation',
+    'deprecated',
     'query_param', 'path_param', 'body', 'header_param',
     'response', 'produces'
 )
 
 
-class OperationDoc(object):
+def deprecated(operation=None):
     """
-    Utility class for building/storing documentation on callback endpoints.
-    """
-
-    #################################################################
-    # Responses
-
-    def add_response(self, status, description, resource=None):
-        if isinstance(status, HTTPStatus):
-            status = status.value
-
-        self.responses[status] = dict_filter(
-            description=description,
-            schema={'$ref': '#/definitions/{}'.format(getmeta(resource).resource_name)} if resource else None
-        )
-
-
-def operation(summary=None, tags=None, deprecated=False):
-    """
-    Decorator for applying operation documentation to a callback.
-
-    The values are based off `Swagger <https://swagger.io/specification>`_.
-
+    Mark an operation deprecated.
     """
     def inner(func):
-        docs = OperationDoc.bind(func)
-        docs.summary = summary
-        docs.tags.update(tags)
-        docs.deprecated = deprecated
+        func.deprecated = True
         return func
-    return inner
+    return inner(operation) if operation else inner
+
+
+def _add_param(name, in_, obj=None, **options):
+    # type: (name, In, **Any) -> None
+    """
+    Add parameter, you should probably use on of :meth:`path_param`, :meth:`query_param`,
+    :meth:`body_param`, or :meth:`header_param`.
+    """
+    def inner(func):
+        data = getattr(func, '_parameters', None)
+        if not data:
+            data = defaultdict(lambda: defaultdict(dict))
+            setattr(func, '_parameters', data)
+
+        dict_filter_update(data[in_][name], options)
+
+        return func
+    return inner(obj) if obj else inner
 
 
 def query_param(name, type, description=None, required=False, default=None,
@@ -60,12 +54,9 @@ def query_param(name, type, description=None, required=False, default=None,
     """
     Query parameter documentation.
     """
-    def inner(func):
-        OperationDoc.bind(func).query_param(
-            name, type, description, required, default, minimum, maximum, enum, **options
-        )
-        return func
-    return inner
+    return _add_param(name, In.Query, type=type, description=description,
+                      minimum=minimum, maximum=maximum, enum=enum,
+                      required=required, default=default, **options)
 
 
 def path_param(name, type, description=None, default=None, minimum=None,
@@ -73,32 +64,25 @@ def path_param(name, type, description=None, default=None, minimum=None,
     """
     Path parameter documentation.
     """
-    def inner(func):
-        OperationDoc.bind(func).path_param(
-            name, type, description, default, minimum, maximum, enum, **options
-        )
-        return func
-    return inner
+    return _add_param(name, In.Path, type=type, description=description,
+                      minimum=minimum, maximum=maximum, enum=enum,
+                      default=default, **options)
 
 
 def body(description=None, default=None, **options):
     """
     Body parameter documentation. 
     """
-    def inner(func):
-        OperationDoc.bind(func).body_param(description, default, **options)
-        return func
-    return inner
+    return _add_param('body', In.Body, description=description,
+                      default=default, **options)
 
 
 def header_param(name, type, description=None, default=None, required=False, **options):
     """
     Header parameter documentation. 
     """
-    def inner(func):
-        OperationDoc.bind(func).header_param(name, type, description, default, required, **options)
-        return func
-    return inner
+    return _add_param(name, In.Header, type=type, description=description,
+                      required=required, default=default, **options)
 
 
 def response(status, description, resource=None):
@@ -108,8 +92,20 @@ def response(status, description, resource=None):
     The values are based off `Swagger <https://swagger.io/specification>`_.
 
     """
+    if isinstance(status, HTTPStatus):
+        status = status.value
+
     def inner(func):
-        OperationDoc.bind(func).add_response(status, description, resource)
+        data = getattr(func, 'responses', None)
+        if not data:
+            data = {}
+            setattr(func, 'responses', data)
+
+        data[status] = dict_filter(
+            description=description,
+            schema={'$ref': '#/definitions/{}'.format(getmeta(resource).resource_name)} if resource else None
+        )
+
         return func
     return inner
 
@@ -122,6 +118,12 @@ def produces(*content_types):
         raise ValueError("In parameter not a valid value.")
 
     def inner(func):
-        OperationDoc.bind(func).produces.update(content_types)
+        data = getattr(func, 'produces', None)
+        if not data:
+            data = set()
+            setattr(func, 'produces', data)
+
+        data.update(content_types)
+
         return func
     return inner
