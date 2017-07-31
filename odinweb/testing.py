8ae69f3ee9c133ac0672e0f8615956bcee973ec4
Tim Savage <tim@savage.company>
2017-08-01 00:44:22 +1000

    Expanded Mock request with codecs

--- odinweb/testing.py
+++ odinweb/testing.py
@@ -5,16 +5,20 @@ Testing Helpers
 Collection of Mocks and Tools for testing APIs.
 
 """
+from odin.codecs import json_codec
 
 
 class MockRequest(object):
     """
     Mocked Request object
     """
-    def __init__(self, query=None, post=None, headers=None, method='GET', body='', host='127.0.0.1'):
+    def __init__(self, query=None, post=None, headers=None, method='GET', body='', host='127.0.0.1',
+                 request_codec=None, response_codec=None):
         self.GET = query or {}
         self.POST = post or {}
         self.headers = headers or {}
         self.method = method
         self.body = body
         self.host = host
+        self.request_codec = request_codec or json_codec
+        self.response_codec = response_codec or json_codec
