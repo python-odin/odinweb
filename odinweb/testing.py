e768348947a1bda1d8123c00719c56da454cdb7c
Tim Savage <tim@savage.company>
2017-08-01 15:30:13 +1000

    Default method to use a Method constant

--- odinweb/testing.py
+++ odinweb/testing.py
@@ -7,12 +7,14 @@ Collection of Mocks and Tools for testing APIs.
 """
 from odin.codecs import json_codec
 
+from odinweb.constants import Method
+
 
 class MockRequest(object):
     """
     Mocked Request object
     """
-    def __init__(self, query=None, post=None, headers=None, method='GET', body='', host='127.0.0.1',
+    def __init__(self, query=None, post=None, headers=None, method=Method.GET, body='', host='127.0.0.1',
                  request_codec=None, response_codec=None):
         self.GET = query or {}
         self.POST = post or {}
