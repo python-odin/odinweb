db555a86fdf7a8557ca58e6e0b01a3881cfe01c7
Tim Savage <tim@savage.company>
2017-08-01 23:39:59 +1000

    Test ListingOperation

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -78,19 +78,74 @@ class TestOperation(object):
 class TestListOperation(object):
     @pytest.mark.parametrize('options, offset, limit', (
         ({}, 0, 50),
-        ({'default_offset': 10}, 10, 50),
         ({'default_limit': 10}, 0, 10),
-        ({'default_offset': 10, 'default_limit': 20}, 10, 20),
     ))
     def test_documentation_applied(self, options, offset, limit):
-        @decorators.ListOperation(**options)
+        @decorators.listing(**options)
         def my_func():
             pass
 
         assert my_func.default_offset == offset
         assert my_func.default_limit == limit
 
-#
+    @pytest.mark.parametrize('options, query, offset, limit, bare', (
+        ({}, {}, 0, 50, False),
+        ({}, {'offset': 10}, 10, 50, False),
+        ({}, {'limit': 20}, 0, 20, False),
+        ({}, {'bare': 'F'}, 0, 50, False),
+        ({}, {'bare': 'T'}, 0, 50, True),
+        ({}, {'bare': 'yes'}, 0, 50, True),
+        ({}, {'offset': 10, 'limit': 20, 'bare': '1'}, 10, 20, True),
+        # Max offset/limit
+        ({'max_limit': 100}, {}, 0, 50, False),
+        ({'max_limit': 100}, {'offset': 10, 'limit': 100}, 10, 100, False),
+        # Silly values?
+        ({}, {'offset': -1}, 0, 50, False),
+        ({}, {'limit': -1}, 0, 1, False),
+        ({}, {'limit': 0}, 0, 1, False),
+        ({}, {'offset': -1, 'limit': -1}, 0, 1, False),
+    ))
+    def test_options_handled(self, options, query, offset, limit, bare):
+        mock_request = MockRequest(query=query)
+
+        @decorators.listing(**options)
+        def my_func(request, **kwargs):
+            assert request is mock_request
+            assert kwargs['offset'] == offset
+            assert kwargs['limit'] == limit
+            assert kwargs['foo'] == 'bar'
+            return [1, 2, 3]
+
+        result = my_func(mock_request, {'foo': 'bar'})
+
+        if bare:
+            assert result == [1, 2, 3]
+        else:
+            assert isinstance(result, decorators.Listing)
+            assert result.results == [1, 2, 3]
+            assert result.offset == offset
+            assert result.limit == limit
+            assert result.total_count is None
+
+    def test_returning_total_count(self):
+        mock_request = MockRequest()
+
+        @decorators.listing
+        def my_func(request, foo, offset, limit):
+            assert foo == 'bar'
+            assert offset == 0
+            assert limit == 50
+            return [1, 2, 3], 5
+
+        result = my_func(mock_request, {'foo': 'bar'})
+
+        assert isinstance(result, decorators.Listing)
+        assert result.results == [1, 2, 3]
+        assert result.offset == 0
+        assert result.limit == 50
+        assert result.total_count == 5
+
+
 # # @pytest.mark.parametrize('decorator, definition', (
 # #     (decorators.route, (PathType.Collection, (Method.GET.value,), None)),
 # #     (decorators.resource_route, (PathType.Resource, (Method.GET.value,), None)),
@@ -110,80 +165,3 @@ class TestListOperation(object):
 # #     assert target.route.route_number == decorators._route_count - 1
 # #     assert target.route[1:-1] == definition
 # #     assert target.route[-1] == target
-#
-#
-# class TestListing(object):
-#     # @pytest.mark.parametrize('options, offset, limit', (
-#     #     ({}, 0, 50),
-#     #     ({'default_offset': 10}, 10, 50),
-#     #     ({'default_limit': 10}, 0, 10),
-#     #     ({'default_offset': 10, 'default_limit': 20}, 10, 20),
-#     # ))
-#     # def test_documentation_applied(self, options, offset, limit):
-#     #     @decorators.listing(**options)
-#     #     def my_func():
-#     #         pass
-#     #
-#     #     query = getattr(my_func, '__docs')._parameters[In.Query]
-#     #     assert query['offset']['default'] == offset
-#     #     assert query['limit']['default'] == limit
-#     #     assert not query['bare']['default']
-#
-#     @pytest.mark.parametrize('options, query, offset, limit, bare', (
-#         ({}, {}, 0, 50, False),
-#         ({}, {'offset': 10}, 10, 50, False),
-#         ({}, {'limit': 20}, 0, 20, False),
-#         ({}, {'bare': 'F'}, 0, 50, False),
-#         ({}, {'bare': 'T'}, 0, 50, True),
-#         ({}, {'bare': 'yes'}, 0, 50, True),
-#         ({}, {'offset': 10, 'limit': 20, 'bare': '1'}, 10, 20, True),
-#         # Max offset/limit
-#         ({'max_offset': 10, 'max_limit': 100}, {}, 0, 50, False),
-#         ({'max_offset': 10, 'max_limit': 100}, {'offset': 10, 'limit': 100}, 10, 100, False),
-#         ({'max_offset': 10, 'max_limit': 100}, {'offset': 11, 'limit': 101}, 10, 100, False),
-#         ({'max_offset': 10, 'max_limit': 100}, {'offset': 20, 'limit': 150}, 10, 100, False),
-#         # Silly values?
-#         ({}, {'offset': -1}, 0, 50, False),
-#         ({}, {'limit': -1}, 0, 1, False),
-#         ({}, {'limit': 0}, 0, 1, False),
-#         ({}, {'offset': -1, 'limit': -1}, 0, 1, False),
-#     ))
-#     def test_options_handled(self, options, query, offset, limit, bare):
-#         mock_request = MockRequest(query=query)
-#
-#         @decorators.listing(**options)
-#         def my_func(self, request, **kwargs):
-#             assert request is mock_request
-#             assert kwargs['offset'] == offset
-#             assert kwargs['limit'] == limit
-#             assert kwargs['foo'] == 'bar'
-#             return [1, 2, 3]
-#
-#         result = my_func(None, mock_request, foo='bar')
-#
-#         if bare:
-#             assert result == [1, 2, 3]
-#         else:
-#             assert isinstance(result, decorators.Listing)
-#             assert result.results == [1, 2, 3]
-#             assert result.offset == offset
-#             assert result.limit == limit
-#             assert result.total_count is None
-#
-#     def test_returning_total_count(self):
-#         mock_request = MockRequest()
-#
-#         @decorators.listing
-#         def my_func(self, request, foo, offset, limit):
-#             assert foo == 'bar'
-#             assert offset == 0
-#             assert limit == 50
-#             return [1, 2, 3], 5
-#
-#         result = my_func(None, mock_request, foo='bar')
-#
-#         assert isinstance(result, decorators.Listing)
-#         assert result.results == [1, 2, 3]
-#         assert result.offset == 0
-#         assert result.limit == 50
-#         assert result.total_count == 5
