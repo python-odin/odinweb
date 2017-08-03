7f68a18fd81e493581f1c12073e0ace188c41c52
Tim Savage <tim@savage.company>
2017-08-04 01:02:31 +1000

    Added signing middleware

---
+++ odinweb/middleware/signing.py
@@ -0,0 +1,68 @@
+"""
+Middleware utilising URL signing for authentication.
+"""
+from __future__ import absolute_import
+
+import logging
+
+from typing import Callable
+
+from .. import signing
+from ..exceptions import PermissionDenied, SigningError
+
+logger = logging.getLevelName(__name__)
+
+
+class SignedAuthBase(object):
+    """
+    Middleware to verify a signed request.
+    """
+    priority = 3  # Ensure authentication run early
+
+    def __init__(self, salt_arg='_', max_expiry=None, digest=None):
+        # type: (str, int, Callable) -> None
+        self.salt_arg = salt_arg
+        self.max_expiry = max_expiry
+        self.digest = digest
+
+    def get_secret_key(self, request, path_args):
+        """
+        Hook to fetch set secret key or allow a secret key to be obtained storage.
+        
+        Note!
+        
+        If a user/account or similar object cannot be found you should return `None`. This will
+        cause a :class:`odinweb.exceptions.PermissionDenied` exception with a generic message to
+        be raised so as not to leak information about the validity or a user/account identifier.
+        
+        """
+        raise NotImplementedError
+
+    def pre_dispatch(self, request, path_args):
+        """
+        Pre dispatch hook
+        """
+        secret_key = self.get_secret_key(request, path_args)
+        if not secret_key:
+            raise PermissionDenied('Signature not valid.')
+
+        try:
+            signing.verify_url_path(request.path, request.GET, secret_key)
+        except SigningError as ex:
+            raise PermissionDenied(str(ex))
+
+
+class FixedSignedAuth(SignedAuthBase):
+    """
+    Signed auth middleware that uses a fixed secret key.
+
+    Using a fixed secret key is not recommended.
+
+    """
+    def __init__(self, secret_key, *args, **kwargs):
+        # type: (bytes, *str, **str) -> None
+        super(FixedSignedAuth, self).__init__(*args, **kwargs)
+        self.secret_key = secret_key
+
+    def get_secret_key(self, request, path_args):
+        return self.secret_key
---
+++ tests/test_middleware_signing.py
@@ -0,0 +1,65 @@
+import base64
+import pytest
+
+from odinweb.data_structures import UrlPath
+from odinweb.decorators import Operation
+from odinweb.exceptions import PermissionDenied
+from odinweb.middleware import signing
+from odinweb.testing import MockRequest
+
+
+class TestSignedAuth(object):
+    @pytest.mark.parametrize('uri', (
+        "/foo/bar?_=YJEYWGBKGUVZS&signature=QKUNPLEDOMFVU2NBTEASPR2J4B524KFMG4GMW2NJISVG2RQQVJEA",
+        "/foo/bar?a=1&b=2&_=YJEYWGBKGUVZS&signature=TU773VE25K5UFPHV6DGD5NXT7D74SFZYKVMEB6ZRONK2UXHT72EQ",
+    ))
+    def test_valid_signature(self, uri):
+        request = MockRequest.from_uri(uri)
+
+        target = signing.FixedSignedAuth(base64.b32decode('DEADBEEF'))
+
+        @Operation(url_path=UrlPath.parse('/foo/bar'), middleware=[target])
+        def callback(r):
+            return 'ok'
+
+        actual = callback(request, {})
+
+        assert actual == 'ok'
+
+    def test_no_key(self):
+        request = MockRequest.from_uri('/foo/bar')
+
+        target = signing.FixedSignedAuth(None)
+
+        @Operation(url_path=UrlPath.parse('/foo/bar'), middleware=[target])
+        def callback(request):
+            return 'ok'
+
+        with pytest.raises(PermissionDenied):
+            callback(request, {})
+
+    @pytest.mark.parametrize('url, kwargs', (
+        # Signature missing
+        ("/foo/bar?_=YJEYWGBKGUVZS", {}),
+        # No salt_used
+        ("/foo/bar?_=YJEYWGBKGUVZS&signature=QKUNPLEDOMFVU2NBTEASPR2J4B524KFMG4GMW2NJISVG2RQQVJEA", {'salt_arg': 'x'}),
+        # Expiry time is required.
+        ("/foo/bar?_=YJEYWGBKGUVZS&signature=QKUNPLEDOMFVU2NBTEASPR2J4B524KFMG4GMW2NJISVG2RQQVJEA", {'max_expiry': 10}),
+        # Signature not valid.
+        ("/foo/bar?_=YJEYWGBKGUVZS&signature=QKUNPLEDOMFVU2NBTEASPR2J4B524KFMG4GMW2NJISVG2RQQVJED", {}),
+        # Invalid expiry value
+        ("/foo/bar?signature=LZ7DKPFZ3UTQB3OCABLOMGDXNKAS4GFM5PNFECZV7FHQF5MXFZFQ&expires=zz&_=YJEYWGBKGUVZS", {}),
+    ))
+    def test_error(self, url, kwargs):
+        request = MockRequest.from_uri('/foo/bar')
+
+        target = signing.FixedSignedAuth(base64.b32decode('DEADBEEF'))
+
+        @Operation(url_path=UrlPath.parse('/foo/bar'), middleware=[target])
+        def callback(request):
+            return 'ok'
+
+        with pytest.raises(PermissionDenied):
+            callback(request, {})
+
+
