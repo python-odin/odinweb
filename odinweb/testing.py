8f12410b6fb29fbf4d3c0ba2325c67b61977944f
Tim Savage <tim@savage.company>
2017-08-04 16:31:39 +1000

    Migrate to use ImmutableMultiDict for query params to support multiple values

--- odinweb/testing.py
+++ odinweb/testing.py
@@ -5,6 +5,8 @@ Testing Helpers
 Collection of Mocks and Tools for testing APIs.
 
 """
+from __future__ import absolute_import
+
 from typing import Dict, Any
 
 from odin.codecs import json_codec
@@ -13,7 +15,8 @@ try:
 except ImportError:
     from urlparse import urlparse, parse_qs
 
-from odinweb.constants import Method
+from .constants import Method
+from .data_structures import ImmutableMultiDict
 
 
 class MockRequest(object):
@@ -27,16 +30,15 @@ class MockRequest(object):
                  request_codec=None, response_codec=None):
         # type: (str, Dict[str, str], Dict[str, str], Method, str, Any, Any) -> MockRequest
         scheme, netloc, path, params, query, fragment = urlparse(uri)
-        query = {k: v[0] for k, v in parse_qs(query).items()}  # Hack need to use a multi dict.
-        return cls(scheme, netloc, path, query, post, headers, method, body, request_codec, response_codec)
+        return cls(scheme, netloc, path, parse_qs(query), post, headers, method, body, request_codec, response_codec)
 
     def __init__(self, scheme='http', host='127.0.0.1', path=None, query=None, headers=None, method=Method.GET,
                  post=None, body='', request_codec=None, response_codec=None):
-        # type: (str, str, str, Dict[str, str], Dict[str, str], Dict[str, str], Method, str, Any, Any) -> MockRequest
+        # type: (str, str, str, Dict[str, str], ImmutableMultiDict, Dict[str, str], Method, str, Any, Any) -> MockRequest
         self.scheme = scheme
         self.host = host
         self.path = path
-        self.GET = query or {}
+        self.GET = ImmutableMultiDict(query or {})
         self.headers = headers or {}
         self.method = method
         self.POST = post or {}
