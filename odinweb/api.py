def6715f2b3163a8fbec738c7e35ab8daddb29d2
Tim Savage <tim@savage.company>
2017-08-11 00:55:19 +1000

    Export PathParam

--- odinweb/api.py
+++ odinweb/api.py
@@ -3,3 +3,4 @@ from .containers import *  # noqa
 from .decorators import *  # noqa
 from .exceptions import *  # noqa
 from .helpers import *  # noqa
+from .data_structures import PathParam  # noqa
