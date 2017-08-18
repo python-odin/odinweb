b0d72fb9a1e322f2053b0ae79077af90df233bdc
Tim Savage <tim@savage.company>
2017-08-18 14:29:29 +1000

    Updated to use pkgutil

--- odinweb/__init__.py
+++ odinweb/__init__.py
@@ -5,9 +5,8 @@ Odin Web
 Web APIs utilising Odin for encoding/decoding and validation.
 
 """
-__import__('pkg_resources').declare_namespace(__name__)
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 __authors__ = "Tim Savage"
 __author_email__ = "tim@savage.company"
 __copyright__ = "Copyright (C) 2016-2017 Tim Savage"
-__version__ = "0.1"
