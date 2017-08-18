180268f7ef449a4c3290f0ee0783097b982b7731
Tim Savage <tim@savage.company>
2017-08-18 14:39:04 +1000

    Moved entry point to api

--- odinweb/__init__.py
+++ odinweb/__init__.py
@@ -1,12 +1,1 @@
-"""
-Odin Web
-~~~~~~~~
-
-Web APIs utilising Odin for encoding/decoding and validation.
-
-"""
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
-__authors__ = "Tim Savage"
-__author_email__ = "tim@savage.company"
-__copyright__ = "Copyright (C) 2016-2017 Tim Savage"
--- odinweb/api.py
+++ odinweb/api.py
@@ -1,3 +1,14 @@
+"""
+Odin Web
+~~~~~~~~
+
+Web APIs utilising Odin for encoding/decoding and validation.
+
+"""
+__authors__ = "Tim Savage"
+__author_email__ = "tim@savage.company"
+__copyright__ = "Copyright (C) 2016-2017 Tim Savage"
+
 from .constants import *  # noqa
 from .containers import *  # noqa
 from .decorators import *  # noqa
