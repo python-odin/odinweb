d60c775824344d84aa54bb81ed746ab89f36b31b
Tim Savage <tim@savage.company>
2018-05-09 13:47:34 +1000

    Support plain listing models

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 
 from odinweb import decorators
 from odinweb.constants import *
-from odinweb.data_structures import NoPath, Param
+from odinweb.data_structures import NoPath, Param, HttpResponse
 from odinweb.exceptions import HttpError
 from odinweb.testing import MockRequest
 
@@ -119,7 +119,7 @@ class TestOperation(object):
         assert actual == expected
 
 
-class TestListOperation(object):
+class TestWrappedListOperation(object):
     @pytest.mark.parametrize('options, offset, limit', (
         ({}, 0, 50),
         ({'default_limit': 10}, 0, 10),
@@ -153,7 +153,7 @@ class TestListOperation(object):
     def test_options_handled(self, options, query, offset, limit, bare):
         mock_request = MockRequest(query=query)
 
-        @decorators.ListOperation(**options)
+        @decorators.WrappedListOperation(**options)
         def my_func(request, **kwargs):
             assert request is mock_request
             assert kwargs['offset'] == offset
@@ -175,7 +175,7 @@ class TestListOperation(object):
     def test_returning_total_count(self):
         mock_request = MockRequest()
 
-        @decorators.ListOperation
+        @decorators.WrappedListOperation
         def my_func(request, foo, offset, limit):
             assert foo == 'bar'
             assert offset == 0
@@ -189,6 +189,74 @@ class TestListOperation(object):
         assert result.offset == 0
         assert result.limit == 50
         assert result.total_count == 5
+
+
+class TestListOperation(object):
+    @pytest.mark.parametrize('options, offset, limit', (
+        ({}, 0, 50),
+        ({'default_limit': 10}, 0, 10),
+    ))
+    def test_documentation_applied(self, options, offset, limit):
+        options.setdefault('use_wrapper', False)
+
+        @decorators.listing(**options)
+        def my_func(request):
+            pass
+
+        assert my_func.default_offset == offset
+        assert my_func.default_limit == limit
+
+    @pytest.mark.parametrize('options, query, offset, limit', (
+        ({}, {}, 0, 50),
+        ({}, {'offset': 10}, 10, 50),
+        ({}, {'limit': 20}, 0, 20),
+        ({}, {'offset': 10, 'limit': 20}, 10, 20),
+        # Max limit
+        ({'max_limit': 100}, {}, 0, 50),
+        ({'max_limit': 100}, {'offset': 10, 'limit': 100}, 10, 100),
+        ({'max_limit': 100}, {'offset': 10, 'limit': 102}, 10, 100),
+        # Silly values?
+        ({}, {'offset': -1}, 0, 50),
+        ({}, {'limit': -1}, 0, 1),
+        ({}, {'limit': 0}, 0, 1),
+        ({}, {'offset': -1, 'limit': -1}, 0, 1),
+    ))
+    def test_options_handled(self, options, query, offset, limit):
+        mock_request = MockRequest(query=query)
+
+        @decorators.ListOperation(**options)
+        def my_func(request, **kwargs):
+            assert request is mock_request
+            assert kwargs['offset'] == offset
+            assert kwargs['limit'] == limit
+            assert kwargs['foo'] == 'bar'
+            return [1, 2, 3]
+
+        result = my_func(mock_request, {'foo': 'bar'})
+
+        assert isinstance(result, HttpResponse)
+        assert result.body == '[1, 2, 3]'
+        assert result['X-Page-Offset'] == str(offset)
+        assert result['X-Page-Limit'] == str(limit)
+        assert 'X-Total-Count' not in result.headers
+
+    def test_returning_total_count(self):
+        mock_request = MockRequest()
+
+        @decorators.ListOperation
+        def my_func(request, foo, offset, limit):
+            assert foo == 'bar'
+            assert offset == 0
+            assert limit == 50
+            return [1, 2, 3], 5
+
+        result = my_func(mock_request, {'foo': 'bar'})
+
+        assert isinstance(result, HttpResponse)
+        assert result.body == '[1, 2, 3]'
+        assert result['X-Page-Offset'] == '0'
+        assert result['X-Page-Limit'] == '50'
+        assert result['X-Total-Count'] == '5'
 
 
 class TestResourceOperation(object):
@@ -220,7 +288,7 @@ class TestResourceOperation(object):
 
 
 @pytest.mark.parametrize('decorator, klass, method', (
-    (decorators.listing, decorators.ListOperation, Method.GET),
+    (decorators.listing, decorators.WrappedListOperation, Method.GET),
     (decorators.create, decorators.ResourceOperation, Method.POST),
     (decorators.detail, decorators.Operation, Method.GET),
     (decorators.update, decorators.ResourceOperation, Method.PUT),
